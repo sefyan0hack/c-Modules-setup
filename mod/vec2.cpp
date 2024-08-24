4fd640620750f05c8783fdc989b5386e82cafed4
sefyan0hack <sefyanhack@gmail.com>
2024-08-24 22:15:09 +0100

    add init files

---
+++ mod/vec2.cpp
@@ -0,0 +1,9 @@
+
+export module vec2;
+
+export class Vec2 {
+public:
+    float x, y;
+    Vec2(float x, float y) : x(x), y(y) {}
+    // other members...
+};
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+import vec2;
+
+int main(){
+    Vec2  pos(1,1);
+    std::cout << pos.x << " " << pos.y;
+}
